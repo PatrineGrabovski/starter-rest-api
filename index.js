7a89ad3deeba852a6465cb7be8b0a17d085cc49a
Patrine <patrine.grabovski96@gmail.com>
2023-06-27 14:30:50 -0300

    first commit

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 const express = require('express')
 const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB('perfect-calf-coatCyclicDB')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-// var options = {
-//   dotfiles: 'ignore',
-//   etag: false,
-//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-//   index: ['index.html'],
-//   maxAge: '1m',
-//   redirect: false
-// }
-// app.use(express.static('public', options))
-// #############################################################################
-
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
+app.post('/visitantes/:id', async (req, res) => {
 
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+   const id = req.params.id;
+  const item = await db.collection('visitantes').set(id, req.body);
 
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
+  res.json(item).end();
+});
+
+// Obtem informações de um visitante
+app.get('/visitantes/:id', async (req, res) => {
+  const id = req.params.id;
+  const item = await db.collection('visitantes').get(id);
+
+  res.json(item).end();
+});
+
+// Delete an visitante
+app.delete('/visitantes/:id', async (req, res) => {
+  const item = await db.collection(visitantes).delete(id)
+
   res.json(item).end()
 })
 
